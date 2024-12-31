e60f69756814ee190a78d096e552176f1d0024e5
aj-ski <126924666+aj-ski@users.noreply.github.com>
2024-12-31 23:22:22 +0000

    Finished up html and css, got hidden class working Tried to start on javascript but chromebooks are stupid so inspect element is blocked and I can't

---
+++ script.js
@@ -0,0 +1,4 @@
+let date = document.getElementById("date");
+let birth = document.getElementById("firstLocation");
+let current = document.getElementById("secondLocation");
+let submit = document.getElementsById("submit");
