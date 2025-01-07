cec0bf33493b2feac565587f8347ebf9c9744e34
aj-ski <126924666+aj-ski@users.noreply.github.com>
2025-01-07 13:46:23 +0000

    Javascript cooking now, got input empty check and converting dates into objects for operations

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 let date = document.getElementById("date");
 let birth = document.getElementById("firstLocation");
 let current = document.getElementById("secondLocation");
-let submit = document.getElementsById("submit");
+let submit = document.getElementById("submit");
+
+submit.addEventListener("click", handleData);
+
+
+function handleData() {
+    let inputCheck = inputIsEmpty();
+    if (inputCheck) {
+        alert("Please input all values");
+    }
+    let date1 = new Date(date.value);
+    let date2 = new Date();
+
+    // console.log((date1.getTime() - date2.getTime())/1000)
+
+}
+
+function inputIsEmpty() {
+    let output = false;
+
+    if ((date.value == '') || (birth.value == '') || (current.value == '')) {
+        output = true;
+    }
+
+    console.log(output);
+    return output;
+    
+}
