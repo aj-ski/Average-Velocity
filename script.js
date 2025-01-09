1365b987986f5ee30c4280f214090e85333ebc48
aj-ski <126924666+aj-ski@users.noreply.github.com>
2025-01-09 13:44:28 +0000

    Changed size of output labels, output now unhides itself on submit, changed font

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let date = document.getElementById("date");
 let birth = document.getElementById("firstLocation");
 let current = document.getElementById("secondLocation");
 let submit = document.getElementById("submit");
+let outputHeading = document.getElementById("outputHeading");
+let outputData = document.getElementById("outputData");
 
 submit.addEventListener("click", handleData);
 
@@ -10,12 +12,20 @@ function handleData() {
     let inputCheck = inputIsEmpty();
     if (inputCheck) {
         alert("Please input all values");
+        return 0;
     }
     let date1 = new Date(date.value);
     let date2 = new Date();
 
-    // console.log((date1.getTime() - date2.getTime())/1000)
+    let hours = (date1.getTime() - date2.getTime())/-1000
+    hours /= 360;
+    hours = Math.round(hours);
+    console.log(hours);
 
+
+    outputData.innerHTML = "3";
+    outputHeading.classList.remove("hide");
+    outputData.classList.remove("hide");
 }
 
 function inputIsEmpty() {
@@ -25,7 +35,6 @@ function inputIsEmpty() {
         output = true;
     }
 
-    console.log(output);
     return output;
     
 }
